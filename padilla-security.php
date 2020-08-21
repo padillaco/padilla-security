ac0beea8694a9538758a6a9ed91ffd102dba8579
Adam Hake <adam.hake@padillaco.com>
2020-08-21 10:24:16 -0400

    fix metacomments and require paths

--- padilla-security.php
+++ padilla-security.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Plugin Nmae: Padilla Security
+ * Plugin Name: Padilla Security
  * Plugin Author: Padilla Digital
  * Version: 1.0.0
  * Description: A series of hardening steps for securing WordPress Sites
@@ -9,9 +9,9 @@
 
  namespace PadillaSecurity;
 
- require_once './inc/headers.php';
- require_once './inc/rest-api.php';
- require_once './inc/wp-versions.php';
- require_once './inc/xmlrpc.php';
- require_once './inc/yoast.php';
+ require_once __DIR__ . '/inc/headers.php';
+ require_once __DIR__ . '/inc/rest-api.php';
+ require_once __DIR__ . '/inc/wp-versions.php';
+ require_once __DIR__ . '/inc/xmlrpc.php';
+ require_once __DIR__ . '/inc/yoast.php';
 
